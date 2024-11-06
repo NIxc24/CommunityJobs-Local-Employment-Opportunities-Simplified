20a53ef05bc82a5b6d90aa3d24fc1706a77ecb87
Paul Pastor <paulcedric71@gmail.com>
2024-11-06 12:29:21 +0800

    modified the index/modified action for buttons

---
+++ employer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employer</title>
+ 
+</head>
+<body>
+    <p>hello g11</p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job Board Home</title>
+    <title>Home</title>
  
 </head>
 <body>
     <div class="container">
         <h1>Welcome to Our Job Board</h1>
-        <form action="index.php" method="POST">
+        <form action="employer.php" method="POST">
             <button type="submit" name="user_type" value="employer" class="button">Employer</button>
         </form><br>
-        <form action="redirect.php" method="POST">
+        <form action="jseeker.php" method="POST">
             <button type="submit" name="user_type" value="jobseeker" class="button">Job Seeker</button>
         </form>
     </div>

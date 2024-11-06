39f15eea8e724487fb3f6d387008f54cf2c88bca
Paul Pastor <paulcedric71@gmail.com>
2024-11-06 13:23:19 +0800

    add button

--- employer.php
+++ employer.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employer</title>
- 
 </head>
 <body>
+    <form action="index.php" method = "Get">
+        <button type ="submit">Home</button>
+    </form>
+    <h1>Community Jobs: Local Employment</h1>
+    <h3>Welcome Employers</h3>
     <p>hello g11</p>
 </body>
 </html>

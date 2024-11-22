2acbc598796986f00b9e6eb0d9c038430063d072
pransyk <152396844+pransyk@users.noreply.github.com>
2024-11-22 23:58:30 +0800

    handle form submission, create jobseeker and DB connection

--- apply.php
+++ apply.php
@@ -4,4 +4,18 @@ require_once 'JobSeeker.php';
 
 $sql = "SELECT * FROM jobs";
 $result = $conn->query($sql);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $job_id = $_POST['job_id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    $job_seeker = new JobSeeker($name, $email);
+  
+    $db = new Database();
+  
+    $job_seeker->applyForJob($db, $job_id);
+  
+    $message = "Your application has been submitted successfully!";
+}
 ?>

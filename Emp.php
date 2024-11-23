e2b6c632d6cb5e4b00bbada34d1f7d6d96b5f204
pransyk <152396844+pransyk@users.noreply.github.com>
2024-11-23 22:30:35 +0800

    add deleteJob method

--- Emp.php
+++ Emp.php
@@ -4,5 +4,10 @@ class Employer {
         $job = new Job(0, $job_title, $company_name, $location, $description, $link);
         return $job->save($db);
     }
+  public function deleteJob(Database $db, $job_id) {
+        $job = new Job($job_id, '', '', '', '', '');
+        return $job->delete($db);
+    }
+
 }
 ?>

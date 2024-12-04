431716e7b341bbcd1d090e3d1f011ed68c3523d4
pransyk <152396844+pransyk@users.noreply.github.com>
2024-12-04 14:28:19 +0800

    add functionality to handle job postings

--- employer.php
+++ employer.php
@@ -7,4 +7,15 @@ $db = new Database();
 
 $employer = new Employer();
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['job_title'])) {
+    $job_title = $_POST['job_title'];
+    $company_name = $_POST['company_name'];
+    $location = $_POST['location'];
+    $description = $_POST['description'];
+    $email = $_POST['email'];
+    $job_seeker_type = $_POST['job_seeker_type'];
+
+    $employer->postJob(db: $db, job_title: $job_title, company_name: $company_name, location: $location, description: $description, email: $email, job_seeker_type: $job_seeker_type);
+    echo "Job posted successfully!";
+}
 ?>
